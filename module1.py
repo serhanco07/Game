276ac4bf82ba184cd3872da2db085cb72b22413a
serhanco07 <43181959+serhanco07@users.noreply.github.com>
2018-11-14 17:01:44 -0500

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,12 @@
+import time, turtle
+def intro():
+    print("Welcome to Star rescue game! This is my first graphical game!")
+    time.sleep(3)
+    print("This is the star we are entering to fight other aliens with our spaceship: ")
+def enter():
+    print("Ok...")
+    time.sleep(1)
+    print("We are entering the star")
+    time.sleep(1)
+    print("Now it is lightspeed modeeee !!!!!!")
+    time.sleep(3)
